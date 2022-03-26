e0eb66f833e69f1b7a554aa2042329f85685e1f3
StuMartin22 <stuart.martin@rocketmail.com>
2022-03-26 16:37:41 -0500

    added more classes, IDs and Paragraphs for further testing / creating a title and rules paragraph.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,24 @@
 var rootEl = $('#root');
 
-var welcomeTxt = document.querySelector('.lrgTitle')
-var rules = document.querySelector('.intro-Rules')
+var bigTitle = document.querySelector('.mainTitle')
+var gameRules = document.querySelector('.rules')
 var beginGame = document.querySelector('.start-btn')
 var timeLeft = 5
 var blank = document.querySelector('#blankSpace')
 var timeDisplay = document.querySelector('.timerStart')
 var quest = document.querySelector('.questions')
 var testers = $('#test')
+var sectionOne = $('#sectionOne')
+
+var titleActual = $('<h1>')
+var gameRulesParagraph = $('<p>')
+
+sectionOne.append(titleActual);
+sectionOne.append(gameRulesParagraph);
+
+titleActual.text('TEST YOUR KNOWLEDGE');
+gameRulesParagraph.text("Rules: This is a timed trivia game! You are going to see a series of questions which will have multiple choice answers. Select the answer you think is correct! If you answer correctly, congratulations, you can move on to the next question! But if you answer incorrectly.. you will lose time off the clock! Good luck!")
+
 beginGame.addEventListener('click', function () {
     var timer = setInterval(function () {
         timeLeft--;

ab634a7ad15ca31f7db691ac9a951c58c9ef5e0a
StuMartin22 <stuart.martin@rocketmail.com>
2022-03-27 11:43:28 -0500

    modified notation format in JS file, added questionSection div. began working on an on click action to hide display elements -- notated out.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,10 @@
-var rootEl = $('#root'); // creates root element 
+// creates root element 
+var rootEl = $('#root'); 
 
-var bigTitle = $('.mainTitle') // beginning of variable definitions
-var gameRules = $('.rules') //E.g.line defines variable bigTitle as .mainTitle class from HTML
+// beginning of variable definitions
+//E.g.line defines variable bigTitle as .mainTitle class from HTML
+var bigTitle = $('.mainTitle') 
+var gameRules = $('.rules') 
 var beginGame = $('.start-btn')
 var timeLeft = 5
 var timeDisplay = $('.timerStart')
@@ -20,33 +23,61 @@ var buttonList = $('<ol>')
 var buttonOne = $('<li>')
 var buttonTwo = $('<li>')
 var buttonThree = $('<li>')
-var buttonFour = $('<li>') // end of variable definitions
+var buttonFour = $('<li>') 
+var questionSection = $('#questionSection')
+var hideList = false
+// end of variable definitions
 
-$(buttons).append(buttonList); // begin appending children elements to parent element 
-$(buttonList).append(buttonOne); // line 1 appends child element buttonList to parent element buttons
+// The following begin appending children elements to parent element 
+// line 1 appends child element buttonList to parent element buttons
+$(buttons).append(buttonList); 
+$(buttonList).append(buttonOne); 
 $(buttonList).append(buttonTwo);
 $(buttonList).append(buttonThree);
 $(buttonList).append(buttonFour);
 
 $(sectionOne).append(titleActual);
 $(sectionOne).append(gameRulesParagraph);
-$(sectionOne).append(beginGame); // end of appending elements list
+$(sectionOne).append(beginGame); 
+// end of appending elements list
 
-titleActual.text('TEST YOUR KNOWLEDGE'); // changes text within variable
-gameRulesParagraph.text("Rules: This is a timed trivia game! You are going to see a series of questions which will have multiple choice answers. Select the answer you think is correct! If you answer correctly, congratulations, you can move on to the next question! But if you answer incorrectly.. you will lose time off the clock! Good luck!") //changes text of variable
+// changes text within variable
+titleActual.text('TEST YOUR KNOWLEDGE'); 
+gameRulesParagraph.text("Rules: This is a timed trivia game! You are going to see a series of questions which will have multiple choice answers. Select the answer you think is correct! If you answer correctly, congratulations, you can move on to the next question! But if you answer incorrectly.. you will lose time off the clock! Good luck!") 
+// end of changes to text of variable
 
-$(beginGame).click(function () { // begins game on button click - button click starts function
-    var timer = setInterval(function () { // variable timer defined in function and begins an interval
-        timeLeft--; // takes timeLeft and deducts time
-        if (timeLeft < 0) { // if timeLeft is less than 0 then we will clear the timer to prevent it from counting into the negatives
+// THE FOLLOWING DESCRIBES BEGIN GAME ON CLICK BELOW
+// begins game on button click / click starts function
+// timer variable defined within function, begins timer
+// takes time left and deducts time --;
+// if timeLeft is less than 0 then we will clear the timer to prevent it from counting into the negatives
+// timedisplay will show time left
+// timedisplay text disappears when timer hits 0
+// if there is time on the clock, then the display will show that instead
+// the function counts down every 1 second
+
+$(beginGame).click(function () { 
+    var timer = setInterval(function () { 
+        timeLeft--; 
+        if (timeLeft < 0) { 
             clearInterval(timer)
-        } else if (timeLeft === 0) { // if time is 0 then it will clear the time display.
-            $(timeDisplay).text(timeLeft) // timedisplay will show time left
-            $(timeDisplay).text('') // timedisplay text set to 0 -- when timer hits 0 the clock disappears.
-        } else $(timeDisplay).text(timeLeft) // if there is time on the clock then it will display that.
-} ,1000) //function is counting down every 1 second.
+        } else if (timeLeft === 0) { 
+            $(timeDisplay).text(timeLeft) 
+            $(timeDisplay).text('') 
+        } else $(timeDisplay).text(timeLeft) 
+} ,1000) 
 });
+// End of beginGame 
 
+// $(beginGame).on('click', function () {
+//     if (hideList) {
+//         $(questionSection).css('display','none');
+//         hideList = !hideList;
+//     } else 
+//         { $(questionSection).css('display','block')
+//         hideList = !hideList;
+//         }
+//     });
 
 // beginGame.addEventListener('click', function () {
 //     var timer = setInterval(function () {
